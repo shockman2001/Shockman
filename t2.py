773388f241224864ff4d979b798af0df60340e2d
shockman2001 <78403096+shockman2001@users.noreply.github.com>
2021-03-16 09:38:12 +0600

    задачи на линейный алгоритм

---
+++ t2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+a=3
+b=4
+y=math.sqrt(a**2+b**2)
+P=y+a+b
+print("P=", P)
+
+
+# In[2]:
+
+
+import math
+a=0
+b=3
+y=math.sqrt(a**2+b**2)
+P=y+a+b
+print("P=", P)
+
+
+# In[3]:
+
+
+import math
+a=6
+b=8
+y=math.sqrt(a**2+b**2)
+P=y+a+b
+print("P=", P)
+
+
+# In[4]:
+
+
+import math
+a=9
+b=12
+y=math.sqrt(a**2+b**2)
+P=y+a+b
+print("P=", P)
+
+
+# In[ ]:
+
+
+
+
