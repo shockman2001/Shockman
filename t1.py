8f9b5bff417840c6697b286581749d83947a9ca1
shockman2001 <78403096+shockman2001@users.noreply.github.com>
2021-03-16 09:37:49 +0600

    задачи на линейный алгоритм

---
+++ t1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+A = 0
+B = 0
+C= A
+A= B
+B= C
+print("A", A)
+print("B", B)
+
+
+# In[2]:
+
+
+A = 0
+B = 5
+C= A
+A= B
+B= C
+print("A", A)
+print("B", B)
+
+
+# In[3]:
+
+
+A = 10
+B = 20
+C= A
+A= B
+B= C
+print("A", A)
+print("B", B)
+
+
+# In[4]:
+
+
+A = 10
+B = 10
+C= A
+A= B
+B= C
+print("A", A)
+print("B", B)
+
+
+# In[ ]:
+
+
+
+
