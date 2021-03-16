57e512bd548b0e21a95f4ffc45706aceb9d751cc
shockman2001 <78403096+shockman2001@users.noreply.github.com>
2021-03-16 09:40:35 +0600

    задачи на разветвляющий алгоритм

---
+++ tr7.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+x=input('x=')
+x=int(x)
+y=input('y=')
+y=int(y)
+if x<=0:
+    print("Ошибка")
+elif x>=0:
+    z=y/x
+    print('x=',x)
+    print('y=',y)
+    print('z=',z)
+
+
+# In[2]:
+
+
+import math
+x=input('x=')
+x=int(x)
+y=input('y=')
+y=int(y)
+if x<=0:
+    print("Ошибка")
+elif x>=0:
+    z=y/x
+    print('x=',x)
+    print('y=',y)
+    print('z=',z)
+
+
+# In[3]:
+
+
+import math
+x=input('x=')
+x=int(x)
+y=input('y=')
+y=int(y)
+if x<=0:
+    print("Ошибка")
+elif x>=0:
+    z=y/x
+    print('x=',x)
+    print('y=',y)
+    print('z=',z)
+
+
+# In[ ]:
+
+
+
+
