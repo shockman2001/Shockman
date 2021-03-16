9994e1ed0304a7c0af933a2f6550a61f7cbadec0
shockman2001 <78403096+shockman2001@users.noreply.github.com>
2021-03-16 09:38:41 +0600

    задачи на линейный алгоритм

---
+++ t3.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+import math
+a=1
+b=2
+c=3
+S=a+b
+S=S+c
+print("S=", S)
+
+
+# In[2]:
+
+
+import math
+a=9
+b=0
+c=1
+S=a+b
+S=S+c
+print("S=", S)
+
+
+# In[3]:
+
+
+
+import math
+a=5
+b=6
+c=9
+S=a+b
+S=S+c
+print("S=", S)
+
+
+# In[ ]:
+
+
+
+
