4ad11461494bf075a299994432005fbf003981ae
shockman2001 <78403096+shockman2001@users.noreply.github.com>
2021-03-16 09:39:37 +0600

    задачи на разветвляющий алгоритм

---
+++ tr5.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[2]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[3]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[ ]:
+
+
+
+
