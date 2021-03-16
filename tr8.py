b26b149d969c498d2f924e050fef11feea9b01cd
shockman2001 <78403096+shockman2001@users.noreply.github.com>
2021-03-16 09:40:55 +0600

    задачи на разветвляющий алгоритм

---
+++ tr8.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+a=input('a=')
+a=int(a)
+b=input('b=')
+b=int(b)
+s=a*b
+if s>500:
+    S=s*0.9
+    print('S=',S)
+else:
+    print('s=',s)
+
+
+# In[2]:
+
+
+import math
+a=input('a=')
+a=int(a)
+b=input('b=')
+b=int(b)
+s=a*b
+if s>500:
+    S=s*0.9
+    print('S=',S)
+else:
+    print('s=',s)
+
+
+# In[3]:
+
+
+import math
+a=input('a=')
+a=int(a)
+b=input('b=')
+b=int(b)
+s=a*b
+if s>500:
+    S=s*0.9
+    print('S=',S)
+else:
+    print('s=',s)
+
+
+# In[4]:
+
+
+import math
+a=input('a=')
+a=int(a)
+b=input('b=')
+b=int(b)
+s=a*b
+if s>500:
+    S=s*0.9
+    print('S=',S)
+else:
+    print('s=',s)
+    
+
+
+# In[5]:
+
+
+
+import math
+a=input('a=')
+a=int(a)
+b=input('b=')
+b=int(b)
+s=a*b
+if s>500:
+    S=s*0.9
+    print('S=',S)
+else:
+    print('s=',s)
+
+
+# In[ ]:
+
+
+
+
